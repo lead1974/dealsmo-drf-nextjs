24b17dad67e9dfeb2fbca34ae4559a06305508cd
“lead1974” <email2lead@gmail.com>
2025-01-23 06:29:16 -0800

    @43 URLs and JWT token and cookies test

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,14 +4,14 @@ from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
-from django.views.debug import default_urlconf
+
 
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="DealsMo API",
+        title="DealsMo.com API",
         default_version="v1",
-        description="DealsMoAPI for Real Estate",
+        description="DealsMo.com Management API for Real Estate",
         contact=openapi.Contact(email="email2balda1@gmail.com"),
         license=openapi.License(name="MIT License"),
     ),
@@ -20,10 +20,12 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path(settings.ADMIN_URL, admin.site.urls),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path(settings.ADMIN_URL, admin.site.urls),
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("core_apps.users.urls")),
 ]
 
-admin.site.site_header = "DealsMo Admin"
-admin.site.site_title = "DealsMo Admin Portal"
-admin.site.index_title = "Welcome to DealsMo Admin Portal"
+admin.site.site_header = "DealsMo.com Admin"
+admin.site.site_title = "DealsMo.com Admin Portal"
+admin.site.index_title = "Welcome to DealsMo.com Admin Portal"
